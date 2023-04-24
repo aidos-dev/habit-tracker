7f6b2d41b2751d0a1cc99496fae0d906b353ed59
Optimus <3300532@gmail.com>
2023-04-24 18:32:22 +0600

    update

--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,25 +1,28 @@
-CREATE TABLE user (
+CREATE TABLE user(
     user_id serial not null unique,
-    user_name varchar(50) not null unique,
+    user_name varchar (50) not null unique,
     first_name varchar(50) not null,
     last_name varchar(50) not null,
-    e - mail varchar(255) not null unique,
+    email varchar(255) not null unique,
     password_hash varchar(255) not null
 );
 
-CREATE TABLE habbit (
+CREATE TABLE habbit 
+(
     habbit_id serial not null unique,
     title varchar(255) not null,
     description varchar(255)
 );
 
-CREATE TABLE user_habbit (
+CREATE TABLE user_habbit 
+(
     user_habbit_id serial not null unique,
     user_id int references user (id) on delete cascade not null,
     habbit_id int references habbit (id) on delete cascade not null
 );
 
-CREATE TABLE habbit_tracker (
+CREATE TABLE habbit_tracker 
+(
     habbit_tracker_id serial not null unique,
     user_habbit_id serial not null,
     unit_of_messure varchar(50) not null,
@@ -30,7 +33,8 @@ CREATE TABLE habbit_tracker (
     counter NUMERIC(10,2)
 );
 
-CREATE TABLE reward (
+CREATE TABLE reward 
+(
     reward_id serial not null unique,
     habbit_tracker_id int references habbit_tracker (id) on delete cascade not null,
     title varchar(255) not null,
