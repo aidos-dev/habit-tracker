2f17f8a02a0246840bb45882ce8e0205bb01f718
Optimus <3300532@gmail.com>
2023-04-25 19:04:27 +0600

    migration queries update

--- schema/000001_init.down.sql
+++ schema/000001_init.down.sql
@@ -1,9 +1,5 @@
 DROP TABLE reward;
-
 DROP TABLE user_habbit;
-
 DROP TABLE habbit_tracker;
-
-DROP TABLE users;
-
 DROP TABLE habbit;
+DROP TABLE user_account;
--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,41 +1,33 @@
-CREATE TABLE user(
-    user_id serial not null unique,
+CREATE TABLE user_account(
+    id serial not null unique,
     user_name varchar (50) not null unique,
     first_name varchar(50) not null,
     last_name varchar(50) not null,
     email varchar(255) not null unique,
     password_hash varchar(255) not null
 );
-
-CREATE TABLE habbit 
-(
-    habbit_id serial not null unique,
+CREATE TABLE habbit (
+    id serial not null unique,
     title varchar(255) not null,
     description varchar(255)
 );
-
-CREATE TABLE user_habbit 
-(
-    user_habbit_id serial not null unique,
-    user_id int references user (id) on delete cascade not null,
+CREATE TABLE user_habbit (
+    id serial not null unique,
+    user_id int references user_account (id) on delete cascade not null,
     habbit_id int references habbit (id) on delete cascade not null
 );
-
-CREATE TABLE habbit_tracker 
-(
-    habbit_tracker_id serial not null unique,
+CREATE TABLE habbit_tracker (
+    id serial not null unique,
     user_habbit_id serial not null,
     unit_of_messure varchar(50) not null,
     goal varchar(50) not null,
     frequency varchar(255) not null,
     start_date DATE NOT NULL DEFAULT CURRENT_DATE,
     end_date DATE NOT NULL,
-    counter NUMERIC(10,2)
+    counter NUMERIC(10, 2)
 );
-
-CREATE TABLE reward 
-(
-    reward_id serial not null unique,
+CREATE TABLE reward (
+    id serial not null unique,
     habbit_tracker_id int references habbit_tracker (id) on delete cascade not null,
     title varchar(255) not null,
     description varchar(255)
