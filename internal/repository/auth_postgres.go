4af1ea43176370d8eba86a1bc0b707a901d845f4
Optimus <3300532@gmail.com>
2023-05-10 12:45:27 +0600

    update

--- internal/repository/auth_postgres.go
+++ internal/repository/auth_postgres.go
@@ -15,11 +15,23 @@ func NewAuthPostgres(db *sqlx.DB) *AuthPostgres {
 	return &AuthPostgres{db: db}
 }
 
+// func (r *AuthPostgres) CreateUser(user models.User) (int, error) {
+// 	var id int
+// 	query := fmt.Sprintf("INSERT INTO %s (user_name, first_name, last_name, email, password_hash) values ($1, $2, $3, $4, $5) RETURNING id", usersTable)
+
+// 	row := r.db.QueryRow(query, user.Username, user.FirstName, user.LastName, user.Email, user.Password)
+// 	if err := row.Scan(&id); err != nil {
+// 		return 0, err
+// 	}
+
+// 	return id, nil
+// }
+
 func (r *AuthPostgres) CreateUser(user models.User) (int, error) {
 	var id int
-	query := fmt.Sprintf("INSERT INTO %s (user_name, first_name, last_name, email, password_hash) values ($1, $2, $3, $4, $5) RETURNING id", usersTable)
+	query := "INSERT INTO $1 (user_name, first_name, last_name, email, password_hash) values ($2, $3, $4, $5, $6) RETURNING id"
 
-	row := r.db.QueryRow(query, user.Username, user.FirstName, user.LastName, user.Email, user.Password)
+	row := r.db.QueryRow(query, usersTable, user.Username, user.FirstName, user.LastName, user.Email, user.Password)
 	if err := row.Scan(&id); err != nil {
 		return 0, err
 	}
